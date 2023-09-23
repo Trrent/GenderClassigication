69d5d839294564e5c0d3a2e84a3682e5954037d0
Dinislam <dinislamsayfullin@gmail.com>
2023-09-23 10:54:39 +0300

    add labels with <gender> to frame

--- get_cam_image.py
+++ get_cam_image.py
@@ -1,6 +1,18 @@
 import cv2
 from time import sleep
 
+
+def draw_label(img, text, pos, bg_color):
+    font_face = cv2.FONT_HERSHEY_SIMPLEX
+    scale = 0.6
+    margin = 5
+    text_size = cv2.getTextSize(text, font_face, scale, cv2.FILLED)
+    end_x = pos[0] + text_size[0][0] + margin
+    end_y = pos[1] - text_size[0][1] - margin
+    cv2.rectangle(img, pos, (end_x, end_y), bg_color, cv2.FILLED)
+    cv2.putText(img, text, pos, font_face, scale, (0, 0, 0), 1, cv2.LINE_AA)
+
+
 casc_path = 'haarcascade_frontalface_default.xml'
 face_cascade = cv2.CascadeClassifier(casc_path)
 
@@ -19,13 +31,14 @@ while True:
     faces = face_cascade.detectMultiScale(
         gray,
         scaleFactor=1.1,
-        minNeighbors=5,
+        minNeighbors=1,
         minSize=(30, 30)
     )
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
+        draw_label(frame, '<gender>', (x, y), (0, 255, 0))
     cv2.imshow('Video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
